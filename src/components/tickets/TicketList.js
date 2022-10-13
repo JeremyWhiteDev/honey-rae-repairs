7708fa8af8c9720db93443da9daa68cdfb72a184
Jeremy White <jcwhite414@gmail.com>
2022-10-12 20:51:27 -0500

    completed part 4 of exercise

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./Tickets.css";
 
 export const TicketList = () => {
   const [tickets, setTickets] = useState([]);
@@ -6,8 +7,28 @@ export const TicketList = () => {
   useEffect(
     () => {
       console.log("Initial state of tickets", tickets); // View the initial state of tickets
+      const fetchData = async () => {
+        const response = await fetch(`http://localhost:8088/serviceTickets`);
+        const ticketArray = await response.json();
+        setTickets(ticketArray);
+      };
+      fetchData();
     },
     [] // When this array is empty, you are observing initial component state
   );
-  return <h2>List of Tickets</h2>;
+  return (
+    <>
+      <h2>List of Tickets</h2>
+      <article className="tickets">
+        {tickets.map((ticket) => {
+          return (
+            <section key={ticket.id} className="ticket">
+              <header>{ticket.description}</header>
+              <footer>Emergency: {ticket.emergency ? "❗" : "No"}</footer>
+            </section>
+          );
+        })}
+      </article>
+    </>
+  );
 };
