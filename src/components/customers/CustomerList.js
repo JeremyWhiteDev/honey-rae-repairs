171a9f3008602c106faf7ea7915a466ddc38cf92
Jeremy White <jcwhite414@gmail.com>
2022-10-22 12:29:49 -0500

    completed part 12

---
+++ src/components/customers/CustomerList.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { Customer } from "./Customer";
+import "./Customers.css";
+
+export const CustomerList = () => {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `http://localhost:8088/customers?_expand=user`
+      );
+      const data = await response.json();
+      setCustomers(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <section className="customers">
+      {customers.map((customer) => {
+        return <Customer customer={customer} />;
+      })}
+    </section>
+  );
+};
